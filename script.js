8804da35820292930478822cfd4afde6f234ba42
tsouza07-maker <t.souza07@escola.pr.gov.br>
2026-09-23 08:34:17 -0300

    atualizei o script

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoesCutir = document.querySeletorAll(".curtir");
+botoesCutir.forEach(function (botaoCurtir){
+    let curtir = false;
+    botaoCurtir.addEventListeer("click", curtir);
+function curtir(){
+    const contador = botaoCurtir.querySeletor("span");
+    if(curiu === false){  
+        contador.textContent++;
+        curiu = true;
+} else{
+    contador.textContent--;
+    curtiu = false;
 
+}
+}
+});
